f6fb877845f8d6b49aacc1b4b740e76f33e09531
Melissa Nguyen <nguyen.melissa23@gmail.com>
2017-05-15 11:50:20 -0700

    day1

---
+++ Lectures.playground/Contents.swift
@@ -0,0 +1,23 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//Operators
+
+// Unary, Binary, Ternary
+
+var amICool = false
+amICool = !amICool
+
+var feelGoodAboutMyself = true
+
+feelGoodAboutMyself = amICool ? true : false
+
+var bankAccountBalance = 100
+
+var cashRegisterMessage = bankAccountBalance >= 50 ? "You just bought the item" : "You are broke as a joke"
+
+
+
